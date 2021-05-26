29a3de098ece3626ca676bd84d42cfa949578561
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2021-05-27 02:14:57 +0700

    Add signal handler Used to catch interrupt signal

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include <csignal>
+#include <unistd.h>
+
 #include "Camera.h"
 #include "OmniCamera.h"
 #include "Vision.h"
 #include "FrontCamera.h"
 #include "Serial.h"
 
+void signalHandler(int signum) {
+    std::cout << "Signal was received" << std::endl;
+
+    std::cout << "Doing something" << std::endl;
+
+    exit(signum);
+}
+
 int main() {
+    signal(SIGTERM, signalHandler);
+    signal(SIGINT, signalHandler);
+
     std::cout << "Hello, World!" << std::endl;
 
     Camera::print();
@@ -28,6 +42,15 @@ int main() {
     Serial arduino{};
     arduino.setBaudRate(115200);
     arduino.init();
+
+    std::cout << "Running program in loop" << std::endl;
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
+    while (true) {
+        sleep(10);
+    }
+#pragma clang diagnostic pop
 
     return 0;
 }
